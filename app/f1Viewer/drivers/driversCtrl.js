5edcf285350b3889bdbf0cdb2cf0ff7a2d7ce082
Ramesh Babu Gopalakrishnan <rameshbabugopalakrishnan@gmail.com>
2015-09-07 22:32:30 +1000

    Updated Driver view to show the driver details.

--- app/f1Viewer/drivers/driversCtrl.js
+++ app/f1Viewer/drivers/driversCtrl.js
@@ -1,8 +1,27 @@
-(function(){
+(function () {
     'use strict';
 
     angular.module('f1Viewer.drivers')
-        .controller('driversCtrl', [function() {
+        .controller('driversCtrl', ['$scope', '$http', '$log', 'f1Service', function ($scope, $http, $log, f1Service) {
+            f1Service.getDriverDetails()
+                .success(function (data) {
+                    $scope.drivers = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+                    $scope.yearOfStanding = data.MRData.StandingsTable.season;
+                    $scope.driverDetailsNotFound = false;
+                })
+                .error(function (data, status, headers, config) {
+                    $log.error("Unable to get Driver details");
+                    $scope.drivers = {};
+                    $scope.yearOfStanding = '';
+                    $scope.driverDetailsNotFound = true;
+                });
 
+            $scope.showDriverDetails = function () {
+                if (typeof($scope.driverDetailsNotFound) !== 'undefined') {
+                    return $scope.drivers && ($scope.drivers.length > 0);
+                } else {
+                    return true;
+                }
+            }
         }]);
 }());
--- app/f1Viewer/services/f1Service.js
+++ app/f1Viewer/services/f1Service.js
@@ -1,10 +1,13 @@
-(function(){
+(function () {
     'use strict';
 
     angular.module('f1Viewer')
-        .service('f1Service', ['$http', function($http) {
-           this.getRaceDetails = function() {
+        .service('f1Service', ['$http', function ($http) {
+            this.getRaceDetails = function () {
                 return $http.get('http://ergast.com/api/f1/2015/results/1.json');
-           };
+            };
+            this.getDriverDetails = function () {
+                return $http.get('http://ergast.com/api/f1/2015/driverStandings.json');
+            };
         }]);
 }());
